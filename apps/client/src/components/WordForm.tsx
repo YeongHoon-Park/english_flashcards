5115d54478dcbd17b631e4170a0f818328a1c999
0hun <0hun@0huns-Mac-mini.local>
2026-03-02 17:29:35 +0900

    perf(client): optimize form rendering using react-hook-form
    (cherry picked from commit 5dc206d009fd50082f70900c8ab5a59647fa54e9)

--- apps/client/src/components/WordForm.tsx
+++ apps/client/src/components/WordForm.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-
 import { createWord } from '@/api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { type SubmitHandler, useForm } from 'react-hook-form';
+
+interface WordFormValues {
+  definition: string;
+  term: string;
+}
 
 export const WordForm = () => {
-  const [term, setTerm] = useState('');
-  const [definition, setDefinition] = useState('');
+  const { handleSubmit, register, reset } = useForm<WordFormValues>();
 
   const queryClient = useQueryClient();
 
@@ -13,41 +16,32 @@ export const WordForm = () => {
     mutationFn: createWord,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['words'] });
-      setTerm('');
-      setDefinition('');
+      reset();
     },
   });
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (!term.trim() || !definition.trim()) {
-      return;
-    }
-
-    mutation.mutate({ term, definition });
+  const onSubmit: SubmitHandler<WordFormValues> = (data) => {
+    mutation.mutate(data);
   };
 
   return (
     <form
       className='bg-white shadow-sm mb-8 p-6 border border-slate-100 rounded-2xl w-full max-w-2xl'
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <div className='flex sm:flex-row flex-col gap-4'>
         <input
+          {...register('term', { required: true })}
           className='flex-1 px-4 py-3 border border-slate-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all'
-          onChange={(e) => setTerm(e.target.value)}
           placeholder='예: Apple'
           type='text'
-          value={term}
         />
 
         <input
+          {...register('definition', { required: true })}
           className='flex-1 px-4 py-3 border border-slate-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all'
-          onChange={(e) => setDefinition(e.target.value)}
           placeholder='예: 사과'
           type='text'
-          value={definition}
         />
         <button
           className='bg-blue-600 hover:bg-blue-700 disabled:bg-slate-400 px-6 py-3 rounded-xl font-bold text-white transition-colors'
